ffcf7292535f92a4a41629cb8c061e2273b7c8cb
Sherin-V <sherinv.de@gmail.com>
2025-06-06 21:41:14 +0200

    New Login UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Login from "./componets/FormContainer/Signup"; // Importing the complete login/signup page
 
@@ -9,17 +8,5 @@ const App = () => {
     </div>
   );
 };
-=======
-function App() {
-  return (
-    <div className="App">
-
-    <h1>Hello World</h1> 
-    
-
- </div>
-  );
-}
->>>>>>> 1f906929260ca25a2fb1faa856e2d215cb84f955
 
 export default App;
---
+++ client/src/componets/FormContainer/Signup.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import "./Signup.css";
+
+
+ // Import CSS file
+
+const Login = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  return (
+    <div className={`container ${isSignUp ? "sign-up-mode" : ""}`}>
+      {/* Forms Container */}
+      <div className="forms-container">
+        <div className="signin-signup">
+          {/* Sign-In Form */}
+          <form className="sign-in-form">
+            <h2 className="title">Sign in</h2>
+            <div className="input-field">
+              <i className="fas fa-user"></i>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="input-field">
+              <i className="fas fa-lock"></i>
+              <input type="password" placeholder="Password" />
+            </div>
+            <input type="submit" value="Login" className="btn solid" />
+            <p className="social-text">Or Sign in with social platforms</p>
+            <div className="social-media">
+              <button  className="social-icon"><i className="fab fa-facebook-f"></i></button>
+              <button  className="social-icon"><i className="fab fa-twitter"></i></button>
+              <button  className="social-icon"><i className="fab fa-google"></i></button>
+              <button  className="social-icon"><i className="fab fa-linkedin-in"></i></button>
+            </div>
+          </form>
+
+          {/* Sign-Up Form */}
+          <form className="sign-up-form">
+            <h2 className="title">Sign up</h2>
+            <div className="input-field">
+              <i className="fas fa-user"></i>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="input-field">
+              <i className="fas fa-envelope"></i>
+              <input type="email" placeholder="Email" />
+            </div>
+            <div className="input-field">
+              <i className="fas fa-lock"></i>
+              <input type="password" placeholder="Password" />
+            </div>
+            <input type="submit" className="btn" value="Sign up" />
+            <p className="social-text">Or Sign up with social platforms</p>
+            <div className="social-media">
+              <a href="https://sherin.fun" className="social-icon"><i className="fab fa-facebook-f"></i></a>
+              <a href="https://sherin.fun" className="social-icon"><i className="fab fa-twitter"></i></a>
+              <a href="https://sherin.fun" className="social-icon"><i className="fab fa-google"></i></a>
+              <a href="https://sherin.fun" className="social-icon"><i className="fab fa-linkedin-in"></i></a>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      {/* Panels Container */}
+      <div className="panels-container">
+        {/* Left Panel */}
+        <div className="panel left-panel">
+          <div className="content">
+            <h3>New here?</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <button className="btn transparent" onClick={() => setIsSignUp(true)}>Sign up</button>
+          </div>
+          <img src="./log.svg"className="image" alt="Sign-up illustration" />
+        </div>
+
+        {/* Right Panel */}
+        <div className="panel right-panel">
+          <div className="content">
+            <h3>One of us?</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <button className="btn transparent" onClick={() => setIsSignUp(false)}>Sign in</button>
+          </div>
+          <img src=".\register.svg" className="image" alt="Sign-in illustration" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
